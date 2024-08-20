3b00ac0d04ba7b5f2a2c5d24a0e7aa3ef73263fe
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-08-20 02:37:09 +0000

    hw cpp

---
+++ dsa/basics/LearnCpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    std::cout<< "Hello world!\n";
+    std:: cout<< "Enter your name: ";
+    std::cin>>name;
+    std::cout<<name;
+    return 0;
+}
