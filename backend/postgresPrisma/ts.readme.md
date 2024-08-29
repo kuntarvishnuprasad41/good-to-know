1bd928aa06d262a584a929cc6b2984c6b79c18c9
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-08-29 12:46:20 +0530

    "Added **/dist/ to .gitignore"

---
+++ backend/postgresPrisma/ts.readme.md
@@ -0,0 +1,17 @@
+## TSC Initializing 
+
+1. - npx tsc --init
+
+``` 
+npx tsc --init 
+```
+
+2. Create ts file 
+
+
+3. open tsconfig.json and modify the following. 
+
+```
+
+
+```
---
+++ backend/postgresPrisma/tsapp/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Success"); //
+});
+
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Failed to start server:", error);
+}
