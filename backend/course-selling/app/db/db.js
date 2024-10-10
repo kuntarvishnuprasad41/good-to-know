52a78f307cc8b14665af07801284ddc9934f3a66
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-10 12:49:21 +0530

    App structure

--- backend/course-selling/app/db/db.js
+++ backend/course-selling/app/db/db.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId
 
-mongoose.connect("mongodb://localhost:27017/course-selling", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-});
+
 
 
 const userSchema = new Schema({
--- backend/course-selling/app/index.js
+++ backend/course-selling/app/index.js
@@ -2,35 +2,29 @@ const express = require('express');
 const { formatOutput } = require('./utils/normalizer');
 const v1Router = require('./routes/v1Routes');
 const app = express();
+const mongoose = require('mongoose');
 
-
-
-
-
-app.use(express.json()); // To parse JSON bodies
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
-
 
 app.get('/', (req, res) => {
     formatOutput(res, 200, "Hello World")
 });
 
-
 app.use('/v1', v1Router);
 
+async function main() {
+    mongoose.connect("mongodb://localhost:27017/course-selling").then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(8000, (err) => {
+            if (!err) {
+                console.log("Running at 8000");
 
+            }
+        })
+    }).catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+}
 
-
-
-
-
-
-app.listen(8000, (err) => {
-    if (!err) {
-        console.log("Running at 8000");
-
-    }
-})
+main()
