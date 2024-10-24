0db5f30256a188f56ff4e53f5a63970526db1075
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-24 06:57:24 +0530

    Signup and In

--- backend/mongo-v1/index.js
+++ backend/mongo-v1/index.js
@@ -1,11 +1,64 @@
 const express = require('express');
+const { UserModel } = require('./db');
+const { default: mongoose } = require('mongoose');
 const app = express();
+const jwt = require('jsonwebtoken')
+
+const jwtpass = '12312321321321321sdfdsfsf'
+
+
+app.use(express.json())
+
+
+mongoose.connect(
+    "mongodb://localhost:27017/v1"
+);
+
+app.post('/signup', async (req, res) => {
+
+    const { name, email, password } = req.body
+
+    await UserModel.create({
+        name: name,
+        email: email,
+        password: password
+    })
+
+    return res.json({
+        message: 'SignUp Successfull'
+    })
+})
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+
+    if (user) {
 
 
 
-app.post('/signup', (req, res) => { })
+        const token = jwt.sign({
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }, jwtpass)
 
-app.post('/signin', (req, res) => { })
+        return res.json({
+            token
+        })
+
+    } else {
+        return res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
+
+})
 
 
 app.post('/todo', (req, res) => { })
