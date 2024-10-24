835c6d7b3125ca157c92ff29a5f6fde5328c277d
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-10-24 06:21:19 +0530

    Init

---
+++ backend/mongo-v1/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+
+app.post('/signup', (req, res) => { })
+
+app.post('/signin', (req, res) => { })
+
+
+app.post('/todo', (req, res) => { })
+
+app.get('/todos', (req, res) => { })
+
+
+app.listen(3000)
---
+++ backend/mongo-v1/readme.md
@@ -0,0 +1,7 @@
+## Steps to run 
+
+1. Create and run a local mongo setup
+
+```
+ docker run -d -p 27017:27017 -v learn-mongo:/data/db mongo
+```
